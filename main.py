c963fadc162832ce0c394f72c98ebf5453c99c6b
clement-gresh <clem_paname@hotmail.com>
2021-12-15 13:30:48 +0100

    feature : configuration pour OpenCV

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import cv2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def openCVcheck(name):
+    print(f'Hi, {name}')
+    print("Your OpenCV version is: " + cv2.__version__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    openCVcheck('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+

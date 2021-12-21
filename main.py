e86eae42188d8a01e87d94afe6d1acddac2e4bd7
clement-gresh <clem_paname@hotmail.com>
2021-12-21 11:28:57 +0100

    feature : maj du .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
 
-def openCVcheck(name):
+
+def opencvcheck(name):
     print(f'Hi, {name}')
     print("Your OpenCV version is: " + cv2.__version__)
 
+
 if __name__ == '__main__':
-    openCVcheck('PyCharm')
+    opencvcheck('PyCharm')
 
 

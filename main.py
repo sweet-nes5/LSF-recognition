4ea1a62978819f9aa44a292a923fc29defaeb048
clement-gresh <clem_paname@hotmail.com>
2022-02-04 13:01:09 +0100

    enhancement : classe HandTracker mise dans un module separe + correction de la plupart des warnings.

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-import cv2
+import time
+from HandTracker import *
 
 
-def opencvcheck(name):
-    print(f'Hi, {name}')
-    print("Your OpenCV version is: " + cv2.__version__)
+def main():
+    previous_time = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandTracker()
+    while True:
+        success, img = cap.read()
+        img = detector.hand_detection(img)
+        lm_list = detector.find_position(img)
+        if len(lm_list) != 0:
+            print(lm_list[4])
+
+        current_time = time.time()
+        fps = 1 / (current_time - previous_time)
+        previous_time = current_time
+        cv2.putText(img,
+                    str(int(fps)),
+                    (10, 70),  # position of the text
+                    cv2.FONT_HERSHEY_COMPLEX,  # font of the text_
+                    3,  # scale
+                    (255, 255, 255),  # color
+                    3  # the font size
+                    )
+        cv2.imshow("image", img)
+        cv2.waitKey(1)
 
 
-if __name__ == '__main__':
-    opencvcheck('PyCharm')
-
-
+if __name__ == "__main__":
+    main()

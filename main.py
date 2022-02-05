23774707427da4dddcd0bc53040231aa51a92bd9
clement-gresh <clem_paname@hotmail.com>
2022-02-05 16:41:12 +0100

    feature : creation de la branche handTracking

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ from HandTracker import *
 def main():
     previous_time = 0
     cap = cv2.VideoCapture(0)
-    detector = HandTracker()
+    # detector = HandTracker()
     while True:
         success, img = cap.read()
+        """
         img = detector.hand_detection(img)
         lm_list = detector.find_position(img)
         if len(lm_list) != 0:
             print(lm_list[4])
-
+        """
         current_time = time.time()
         fps = 1 / (current_time - previous_time)
         previous_time = current_time
         cv2.putText(img,
                     str(int(fps)),
                     (10, 70),  # position of the text
-                    cv2.FONT_HERSHEY_COMPLEX,  # font of the text_
+                    cv2.FONT_HERSHEY_COMPLEX,  # font of the text
                     3,  # scale
                     (255, 255, 255),  # color
                     3  # the font size
